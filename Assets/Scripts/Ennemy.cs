db5679588753ac0b13028101cbf3c29331c88154
Promethee Toneatti <promethee.toneatti@telecom-sudparis.eu>
2022-03-19 12:09:55 +0100

    deplacement du player en rythme

--- Assets/Scripts/Ennemy.cs
+++ Assets/Scripts/Ennemy.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Ennemy : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private float speed = 10;
+    float h_input;
+    float v_input;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector2(h_input, v_input).normalized * speed;
     }
+
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,43 +8,76 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 10;
     float h_input;
     float v_input;
+    bool canMove;
+    float time;
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
+        //StartCoroutine(FreezePlayer(time));
     }
 
     // Update is called once per frame
     void Update()
     {
-        v_input = 0;
-        h_input = 0;
-        Deplacement();
+        time = time + Time.deltaTime;
+        GetInput();
+        if (time >= 1)
+        {
+            
+            Deplacement();
+            h_input = 0;
+            v_input = 0;
+            time = time % 1;
+        }
+             
+        //GetInput();
+        //Deplacement();
+        print(h_input);
+
+
 
     }
-
-    void Deplacement()
+    void GetInput()
     {
         if (Input.GetKey(KeyCode.Z))
         {
-            v_input += 1;
+            v_input = 1;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            v_input -= 1;
+            v_input = -1;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            h_input += 1;
+            h_input = 1;
         }
 
         if (Input.GetKey(KeyCode.Q))
         {
-            h_input -= 1;
+            h_input = -1;
         }
-        rb.velocity = new Vector2(h_input, v_input).normalized * speed;
+    }
+    void Deplacement()
+    {
+        transform.Translate(h_input, v_input, 1);
+        
+            
+        //rb.velocity = new Vector2(h_input, v_input).normalized * speed;
 
+    }
+
+    private IEnumerator FreezePlayer(float time)
+    {
+        for(; ; )
+        {
+            canMove = false;
+            yield return new WaitForSeconds(time);
+            canMove = true;
+            yield return new WaitForSeconds(time);
+        }
+        
     }
 }
