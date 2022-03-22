70697c2df2bc9d4e859556ae2ca77297d38eb687
cmeignan <clement.meignant@laposte.net>
2022-03-22 11:40:42 +0100

    corr death replacement

--- Assets/EscapeAnime.cs
+++ Assets/EscapeAnime.cs
@@ -6,7 +6,6 @@ using TMPro;
 public class EscapeAnime : MonoBehaviour
 {
     [SerializeField] TMP_Text _text;
-    [SerializeField] TMP_Text _text2;
 
     private void Awake()
     {
@@ -32,16 +31,4 @@ public class EscapeAnime : MonoBehaviour
         _text.color = new Color(_text.color.r, _text.color.g, _text.color.b, 1f);
     }
 
-    private IEnumerator FadeOut2()
-    {
-        _text2.gameObject.SetActive(true);
-        while (_text2.color.a > 0)
-        {
-
-            _text2.color = new Color(_text2.color.r, _text2.color.g, _text2.color.b, _text2.color.a - Time.unscaledDeltaTime / 4f);
-            yield return null;
-        }
-        _text2.gameObject.SetActive(false);
-        _text2.color = new Color(_text2.color.r, _text2.color.g, _text2.color.b, 1f);
-    }
 }
