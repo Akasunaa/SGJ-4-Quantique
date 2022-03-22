9bdbca2275589686759fd95c151fc60205d55dd9
cmeignan <clement.meignant@laposte.net>
2022-03-22 11:18:44 +0100

    pushpush

--- Assets/ManagerUI.cs
+++ Assets/ManagerUI.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ManagerUI : MonoBehaviour
 {
     [SerializeField] GameObject[] cartes;
+    [SerializeField] GameObject[] _miniCartes;
+    [SerializeField] GameObject[] _miniCartesButton;
     [SerializeField] GameObject _backButton;
     [SerializeField] GameObject _nerdButton;
     [SerializeField] GameObject _happyButton;
@@ -15,12 +18,38 @@ public class ManagerUI : MonoBehaviour
 
     public bool BigCardOpen { get { return (_bigCardOpen); } }
 
+    private void Awake()
+    {
+        Zone.NewZone += NewZoneEvent;
+    }
+
     private void OnEnable()
     {
         _backButton.SetActive(false);
         foreach (GameObject carte in cartes)
         {
             carte.SetActive(false);
+        }
+    }
+
+    public void NewZoneEvent(int nbZone)
+    {
+        if (nbZone == 2)
+        {
+            _miniCartes[0].SetActive(true);
+            _miniCartes[1].SetActive(true);
+            _miniCartesButton[0].GetComponent<Button>().interactable = true;
+            _miniCartesButton[1].GetComponent<Button>().interactable = true;
+        }
+        else if (nbZone == 3)
+        {
+            _miniCartes[2].SetActive(true);
+            _miniCartesButton[2].GetComponent<Button>().interactable = true;
+        }
+        else if (nbZone == 4)
+        {
+            _miniCartes[3].SetActive(true);
+            _miniCartesButton[3].GetComponent<Button>().interactable = true;
         }
     }
 
